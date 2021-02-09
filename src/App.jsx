ba2956d6a2e3d6311d8fc1d69927bb85044196c6
Zoe Zhan <zoezhan@Zoes-MacBook.local>
2021-02-09 15:21:14 +1000

    fix useEffect dependency warning

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,30 @@ function App() {
   }, []);
 
   useEffect(() => {
+    const filterHandler = () => {
+      switch (status) {
+        case "Completed":
+          setFilteredTodos(todos.filter((todo) => todo.completed === true));
+          break;
+        case "Uncompleted":
+          setFilteredTodos(todos.filter((todo) => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    };
+
+    const saveLocalTodos = () => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    };
+
     filterHandler();
     saveLocalTodos();
   }, [todos, status]);
 
   // save to local
-  const saveLocalTodos = () => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  };
+
   const getLocalTodos = () => {
     if (localStorage.getItem("todos") === null) {
       localStorage.setItem("todos", JSON.stringify([]));
@@ -52,19 +68,6 @@ function App() {
   //for TodoList
 
   //for form
-  const filterHandler = () => {
-    switch (status) {
-      case "Completed":
-        setFilteredTodos(todos.filter((todo) => todo.completed === true));
-        break;
-      case "Uncompleted":
-        setFilteredTodos(todos.filter((todo) => todo.completed === false));
-        break;
-      default:
-        setFilteredTodos(todos);
-        break;
-    }
-  };
 
   const statusHandler = (e) => {
     setStatus(e.target.textContent);
